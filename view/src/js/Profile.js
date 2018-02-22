c269d7acc6026096eb8419f3d9cb297bb1722b5f
papistan <jpapisan@gmail.com>
2018-02-22 10:45:18 -0800

    Increases token name font size

--- view/src/js/Profile.js
+++ view/src/js/Profile.js
@@ -58,25 +58,25 @@ class Profile extends Component {
                         <Navigation />
                         <Grid>
             				<Row>
-            					<Col xs={10}><h2>{token.name}</h2></Col>
+            					<Col sm={10} style={{ fontWeight: 'bold' }}><h1>{token.name}</h1></Col>
             				</Row>
             				<Row>
-            					<Col xs={10}><p style={{ color: '#b19cd9', fontSize: "16px",textTransform: "capitalize" }}>Category: {token.category}</p>
+            					<Col sm={10}><p style={{ color: '#b19cd9', fontSize: "16px",textTransform: "capitalize" }}>Category: {token.category}</p>
                                             <p style={{ color: '#b19cd9', marginTop: "-10px", fontSize: "16px" }}>Age: {token.age}</p>
                                 </Col>
             				</Row>
             				<Row>
-            					<Col xs={10}><p>{token.description}</p></Col>
+            					<Col sm={10}><p>{token.description}</p></Col>
             				</Row>
             				<hr></hr>
             				<Row>
-            					<Col xs={8} className="inline">
+            					<Col sm={8} className="inline">
             						<div>
             							<h2 className='inline' style={{ fontWeight: 'bold' }}>{token.score_overall} </h2>
             							<p className='inline' style={{ color: 'grey', fontWeight:"bold" }}>/100 overall score</p>
             						</div>
             					</Col>
-            					<Col xs={4} className="inline">
+            					<Col sm={4} className="inline">
             						{(token.score_overall > 50) ?
         	                    		<p style={{ fontSize: "16px" }}><Label className="trusted">
                                                 <Glyphicon glyph="ok-circle" /> Trusted
@@ -88,73 +88,73 @@ class Profile extends Component {
             					</Col>
             				</Row>
             				<Row>
-            					<Col xs={12}>
+            					<Col sm={12}>
             						<p style={{ fontWeight: "bold" }}>From {reviews.length} User Reviews</p>
             					</Col>
             				</Row>
             				<Row>
-            					<Col xs={7}>
+            					<Col sm={7}>
             						<ProgressBar bsStyle="warning" now={token.score_transparency} />
             					</Col>
-            					<Col xs={5}>
+            					<Col sm={5}>
             						<span className="text-nowrap inline"><p style={{ color: '#FFCC11' }}> Transparency: <b>{token.score_transparency}%</b></p></span>
             					</Col>
             				</Row>
             				<Row>
-            					<Col xs={7}>
+            					<Col sm={7}>
             						<ProgressBar bsStyle="warning" now={token.score_legal} />
             					</Col>
-            					<Col xs={5}>
+            					<Col sm={5}>
             						<span className="text-nowrap inline"><p style={{ color: '#FFCC11' }}> Legal Status: <b>{token.score_legal}%</b></p></span>
             					</Col>
             				</Row>
             				<Row>
-            					<Col xs={7}>
+            					<Col sm={7}>
             						<ProgressBar bsStyle="warning" now={token.score_functionality} />
             					</Col>
-            					<Col xs={5}>
+            					<Col sm={5}>
             						<span className="text-nowrap inline"><p style={{ color: '#FFCC11' }}> Functionality: <b>{token.score_functionality}%</b></p></span>
             					</Col>
             				</Row>
             				<Row>
-            					<Col xs={7}>
+            					<Col sm={7}>
             						<ProgressBar bsStyle="warning" now={token.score_governance} />
             					</Col>
-            					<Col xs={5}>
+            					<Col sm={5}>
             						<span className="text-nowrap inline"><p style={{ color: '#FFCC11' }}> Governance: <b>{token.score_governance}%</b></p></span>
             					</Col>
             				</Row>
             				<div className='review-div'>
             					<Row>
-            						<Col xs={7}>
+            						<Col sm={7}>
             							<p style={{ fontSize: '30px' }}>{lastReview.score_overall}</p>
             							<p style={{ color: 'grey' }}>Overall Score</p>
             						</Col>
-            						<Col xs={5}>
+            						<Col sm={5}>
             							<h4>{lastReview.name}</h4>
             							<p>{months[date.getMonth()]} {date.getDate()}, {date.getFullYear()}</p>
             						</Col>
             					</Row>
             					<Row>
-            						<Col xs={3}>
+            						<Col sm={3}>
             							<p>{lastReview.score_transparency}</p>
             							<p style={{ color: 'grey' }}>Transparency</p>
             						</Col>
-            						<Col xs={3}>
+            						<Col sm={3}>
             							<p>{lastReview.score_legal}</p>
             							<span className="text-nowrap"><p style={{ color: 'grey' }}>Legal Status</p></span>
             						</Col>
-            						<Col xs={3}>
+            						<Col sm={3}>
             							<p>{lastReview.score_functionality}</p>
             							<p style={{ color: 'grey' }}>Functionality</p>
             						</Col>
-            						<Col xs={3}>
+            						<Col sm={3}>
             							<p>{lastReview.score_governance}</p>
             							<p style={{ color: 'grey' }}>Governance</p>
             						</Col>
             					</Row>
             					<Row>
-            						<Col xs={12}>
+            						<Col sm={12}>
                                             {(lastReview.review.length > 250) ?
                                                 <p style={{ color: 'grey' }}>{lastReview.review.substr(0,250) + "..."}</p> :
                                                 <p style={{ color: 'grey' }}> {lastReview.review}</p>
@@ -163,7 +163,7 @@ class Profile extends Component {
             					</Row>
             				</div>
         					<Row>
-        						<Col xs={4} xsOffset={8}>
+        						<Col sm={4} xsOffset={8}>
         							<Link 
         								style={{ color: 'grey', fontWeight: 'bold' }}
         								to={'/' + token.name + '/reviews'}>
@@ -173,33 +173,33 @@ class Profile extends Component {
         					</Row>
         					<hr></hr>
         					<Row>
-        						<Col xs={10}>
+        						<Col sm={10}>
         							<h3 style={{ paddingBottom: '20px' }}>General Information</h3>
         						</Col>
         					</Row>
         					<Row>
-        						<Col xs={6}>
+        						<Col sm={6}>
         							<p style={{ color: 'grey' }}>Founder</p>
         						</Col>
-        						<Col xs={6}>
+        						<Col sm={6}>
         							<p>{token.founders}</p>
         						</Col>
         					</Row>
         					<hr></hr>
         					<Row>
-        						<Col xs={6}>
+        						<Col sm={6}>
         							<p style={{ color: 'grey' }}>Symbol</p>
         						</Col>
-        						<Col xs={6}>
+        						<Col sm={6}>
         							<p>{token.symbol}</p>
         						</Col>
         					</Row>
         					<hr></hr>
         					<Row>
-        						<Col xs={6}>
+        						<Col sm={6}>
         							<p style={{ color: 'grey' }}>Website</p>
         						</Col>
-        						<Col xs={6}>
+        						<Col sm={6}>
         							<a style={{ fontWeight: 'bold', color: 'gray' }} href={ "http://" + token.website }>{token.website}</a>
         						</Col>
         					</Row>
