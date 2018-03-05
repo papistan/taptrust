4cbea18f47a650f32a6d8e51c344a2ec33ffa767
David Leong <david0leong@gmail.com>
2018-03-05 07:24:07 -0800

    Update ReviewForm

--- www/src/js/ReviewForm.js
+++ www/src/js/ReviewForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Grid, Button } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
+import Grid from 'react-bootstrap/lib/Grid';
+import Alert from 'react-bootstrap/lib/Alert';
+import Button from 'react-bootstrap/lib/Button';
 
 import { createReviewOfToken } from './api';
 
@@ -31,132 +33,142 @@ class ReviewForm extends Component {
     const { match: { params: { tokenId } } } = this.props;
 
     createReviewOfToken(tokenId, this.state)
-      .then(res => {
+      .then(() => {
         this.setState({ redirectToNewPage: true });
       })
-      .catch(err => {
+      .catch(() => {
         this.setState({ error: 'Error, try again' });
       });
   };
 
   render() {
     const { match: { params: { tokenId } } } = this.props;
+    const {
+      redirectToNewPage,
+      error,
+      name,
+      url,
+      review,
+      score_transparency,
+      score_governance,
+      score_legal,
+      score_functionality
+    } = this.state;
 
-    if (this.state.redirectToNewPage) {
+    if (redirectToNewPage) {
       return <Redirect to="/" />;
     }
 
     return (
-      <div>
-        <Grid>
-          <p style={{ color: 'red' }}>{this.state.error}</p>
-          <h2>Post Review</h2>
-          <p>for token with tokenId: {tokenId}</p>
+      <Grid>
+        {error && <Alert bsStyle="danger">{error}</Alert>}
 
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                required
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-            </div>
+        <h2>Post Review</h2>
+        <p>for token with tokenId: {tokenId}</p>
 
-            <div>
-              <label htmlFor="url">Personal Website/Blog:</label>
-              <p className="inline">http://</p>
-              <input
-                className="inline"
-                type="text"
-                id="url"
-                name="url"
-                required
-                value={this.state.url}
-                onChange={this.handleChange}
-              />
-            </div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              value={name}
+              onChange={this.handleChange}
+            />
+          </div>
 
-            <div>
-              <label htmlFor="review">Review:</label>
-              <textarea
-                rows="3"
-                cols="60"
-                minLength="100"
-                id="review"
-                name="review"
-                required
-                value={this.state.review}
-                onChange={this.handleChange}
-              />
-            </div>
+          <div>
+            <label htmlFor="url">Personal Website/Blog:</label>
+            <p className="inline">http://</p>
+            <input
+              className="inline"
+              type="text"
+              id="url"
+              name="url"
+              required
+              value={url}
+              onChange={this.handleChange}
+            />
+          </div>
 
-            <p style={{ paddingTop: '20px' }}>Scores (0-100):</p>
-            <div>
-              <label htmlFor="score_transparency">Transparency:</label>
-              <input
-                type="number"
-                id="score_transparency"
-                name="score_transparency"
-                min="0"
-                max="100"
-                required
-                value={this.state.score_transparency}
-                onChange={this.handleChange}
-              />
-            </div>
+          <div>
+            <label htmlFor="review">Review:</label>
+            <textarea
+              rows="3"
+              cols="60"
+              minLength="100"
+              id="review"
+              name="review"
+              required
+              value={review}
+              onChange={this.handleChange}
+            />
+          </div>
 
-            <div>
-              <label htmlFor="score_governance">Governance:</label>
-              <input
-                type="number"
-                id="score_governance"
-                name="score_governance"
-                min="0"
-                max="100"
-                required
-                value={this.state.score_governance}
-                onChange={this.handleChange}
-              />
-            </div>
+          <p style={{ paddingTop: '20px' }}>Scores (0-100):</p>
+          <div>
+            <label htmlFor="score_transparency">Transparency:</label>
+            <input
+              type="number"
+              id="score_transparency"
+              name="score_transparency"
+              min="0"
+              max="100"
+              required
+              value={score_transparency}
+              onChange={this.handleChange}
+            />
+          </div>
 
-            <div>
-              <label htmlFor="score_legal">Legal:</label>
-              <input
-                type="number"
-                id="score_legal"
-                name="score_legal"
-                min="0"
-                max="100"
-                required
-                value={this.state.score_legal}
-                onChange={this.handleChange}
-              />
-            </div>
+          <div>
+            <label htmlFor="score_governance">Governance:</label>
+            <input
+              type="number"
+              id="score_governance"
+              name="score_governance"
+              min="0"
+              max="100"
+              required
+              value={score_governance}
+              onChange={this.handleChange}
+            />
+          </div>
 
-            <div>
-              <label htmlFor="score_functionality">Functionality:</label>
-              <input
-                type="number"
-                id="score_functionality"
-                name="score_functionality"
-                min="0"
-                max="100"
-                required
-                value={this.state.score_functionality}
-                onChange={this.handleChange}
-              />
-            </div>
+          <div>
+            <label htmlFor="score_legal">Legal:</label>
+            <input
+              type="number"
+              id="score_legal"
+              name="score_legal"
+              min="0"
+              max="100"
+              required
+              value={score_legal}
+              onChange={this.handleChange}
+            />
+          </div>
 
-            <Button className="btn btn-primary centerButton" type="submit">
-              Send
-            </Button>
-          </form>
-        </Grid>
-      </div>
+          <div>
+            <label htmlFor="score_functionality">Functionality:</label>
+            <input
+              type="number"
+              id="score_functionality"
+              name="score_functionality"
+              min="0"
+              max="100"
+              required
+              value={score_functionality}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <Button className="btn btn-primary centerButton" type="submit">
+            Send
+          </Button>
+        </form>
+      </Grid>
     );
   }
 }
